8a0773fc75b490fb28d737c40ae0882edde4b82b
unknown <dedaniel688@gmail.com>
2022-08-18 04:34:20 -0700

    gg

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Pageroute />} />
+          <Route exact path="" element={<Pageroute />} />
           <Route path="/readmore" element={<ReadMore />} />
         </Routes>
       </Router>
